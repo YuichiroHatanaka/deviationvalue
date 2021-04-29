503d00458ede1741b3b463bcae11d5e3f7698270
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2021-04-29 19:41:16 +0900

    Update d.c

--- d.c
+++ d.c
@@ -11,10 +11,8 @@ double dev;
 
 int main(void){
   printf("母数を入力してください。\n");
-  scanf("%d", &n);
-  
+  scanf("%d", &n); 
   ave = 100 / n;
-  
   a = (100 - ave) * (100 - ave);
   b = (0 - ave) * (0 - ave);
   
@@ -23,14 +21,9 @@ int main(void){
   }
   
   dis = a / n;
-  
   root = sqrt(dis);
-  
   dev = (100 - ave) / root;
-  
   dev = 10 * dev + 50;
-  
   printf("偏差値は%lfです。\n", dev);
-  
   return 0;
 }
